277d8cc243c039a3ccab72827283af16ebc9d923
Артур Бат-Очир <dumparcher@mail.ru>
2025-04-27 12:35:19 +0300

    обработать исключения

--- app/core/route.php
+++ app/core/route.php
@@ -2,86 +2,91 @@
 // что то типа роутинга
 class Route
 {
-	static function start()
-	{
-		// контроллер, модель и действие для index.php(по умолчанию)
-		$controller_name = 'Main';
-		$action_name = 'index';
-		// Массив параметров из URI запроса.
-		$param = null;
-
-	
-		// разделить запрос
-		$routes = explode('/', $_SERVER['REQUEST_URI']);
-		// var_dump($_SERVER['REQUEST_URI']);
-
-		// имя контроллера
-		if ( !empty($routes[1]) )
-		{	
-			$controller_name = $routes[1];
-		}
-		
-		//  имя метода
-		if ( !empty($routes[2]) )
-		{
-			$action_name = $routes[2];
-		}
-
-		if ( !empty($routes[3]) )
-		{
-			$param = $routes[3];
-		}
-		
-		// префиксы
-		$model_name = 'Model_'.$controller_name;
-		$controller_name = 'Controller_'.$controller_name;
-		$action_name = 'action_'.$action_name;
+  static function start()
+  {
+    // контроллер, модель и действие для index.php(по умолчанию)
+    $controller_name = 'Main';
+    $action_name = 'index';
+    // Массив параметров из URI запроса.
+    $param = null;
 
 
-		// подключить файл с классом модели 
-		$model_file = strtolower($model_name).'.php';
-		$model_path = "app/models/".$model_file;
-		if(file_exists($model_path))
-		{
-			include "app/models/".$model_file;
-		}
+    // разделить запрос
+    $routes = explode('/', $_SERVER['REQUEST_URI']);
+    // var_dump($_SERVER['REQUEST_URI']);
 
-		// исключения!!!!
-		// подключить файл с классом контроллера
-		$controller_file = strtolower($controller_name).'.php';
-		$controller_path = "app/controllers/".$controller_file;
-		if(file_exists($controller_path))
-		{
-			include "app/controllers/".$controller_file;
-		}
-		else
-		{
-			// если что перенаправить..
-			Route::ErrorPage404();
-		}
-		
-		// создать контроллер
-		$controller = new $controller_name;
-		$action = $action_name;
-		
-		if(method_exists($controller, $action))
-		{
-			// выполнить действие контроллера
-			$controller->$action($param);
-		}
-		else
-		{
-			// если что перенаправить..
-			Route::ErrorPage404();
-		}
-	
-	}
-	
-	static function ErrorPage404()
-	{
-        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
-        header('HTTP/1.1 404 Not Found');
-		header("Status: 404 Not Found");
-		header('Location:'.$host.'404');
+    // имя контроллера
+    if (!empty($routes[1])) {
+      $controller_name = $routes[1];
     }
+
+    //  имя метода
+    if (!empty($routes[2])) {
+      $action_name = $routes[2];
+    }
+
+    if (!empty($routes[3])) {
+      $param = $routes[3];
+    }
+
+    // префиксы
+    $model_name = 'Model_' . $controller_name;
+    $controller_name = 'Controller_' . $controller_name;
+    $action_name = 'action_' . $action_name;
+
+
+    // подключить файл с классом модели
+    $model_file = strtolower($model_name) . '.php';
+    $model_path = "app/models/" . $model_file;
+    if (file_exists($model_path)) {
+      include "app/models/" . $model_file;
+    }
+
+    // подключить файл с классом контроллера
+    $controller_file = strtolower($controller_name) . '.php';
+    $controller_path = "app/controllers/" . $controller_file;
+    try {
+      if (!file_exists($controller_path)) {
+        throw new \Exception("Файл не найден!");
+      }
+      include "app/controllers/" . $controller_file;
+    } catch (\Exception $e) {
+      echo $e->getMessage();
+    }
+
+    // else
+    // {
+    //   // если что перенаправить..
+    //   Route::ErrorPage404();
+    // }
+
+    // создать контроллер
+    $controller = new $controller_name;
+    $action = $action_name;
+    try {
+      if (!method_exists($controller, $action)) {
+        throw new \Exception("Контроллер не найден!");
+      }
+      // выполнить действие контроллера
+      $controller->$action($param);
+    } catch (\Exception $e) {
+      echo $th->getMessage();
+    }
+    // if (method_exists($controller, $action)) {
+    //   // выполнить действие контроллера
+    //   $controller->$action($param);
+    // } else {
+    //   // если что перенаправить..
+    //   Route::ErrorPage404();
+    // }
+
+  }
+
+  static function ErrorPage404()
+  {
+    $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    header('HTTP/1.1 404 Not Found');
+    header("Status: 404 Not Found");
+    header('Location:' . $host . '404');
+  }
 }
